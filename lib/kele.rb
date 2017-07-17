6eba09eb7a72a4bf3d46553deeea88d713a758eb
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-07-17 12:47:07 -0700

    assignment 4 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,11 +21,17 @@ require 'json'
     end
 
     def get_me
-
       response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth })
       puts response
       @parsed_data = JSON.parse(response.body)
       puts @parsed_data
+    end
+
+    def get_mentor_availability(mentor_id)
+
+      address_mentor = 'https://www.bloc.io/api/v1/mentors/' + mentor_id.to_s + '/student_availability'
+      response = self.class.get(address_mentor, headers: { "authorization" => @auth })
+      @mentor_parse = JSON.parse(response.body)
     end
 
   end
